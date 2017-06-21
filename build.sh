806d796122517a8052edce3754c5e11deba3f904
pouya <pouya@applariat.com>
2017-06-21 13:53:01 -0700

    using /code instead of /src

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 # appLariats generic golang build script
 
-#Log everything in /src/build.log
-logfile=/src/build.log
+#Log everything in /code/build.log
+logfile=/code/build.log
 exec > $logfile 2>&1
 set -x
 
 mkdir -p /go/src/app
-cp -rf /src/* /go/src/app
+cp -rf /code/* /go/src/app
 cd /go/src/app
 
 go-wrapper download
